164593e81b2cb3127f61c260f31f0c00a4d7b468
b3b3r <b3b3rseg@gmail.com>
2021-11-13 17:10:48 +0100

    init project

---
+++ src/main/java/b3b3r/demo/CsvToDbApplication.java
@@ -0,0 +1,13 @@
+package b3b3r.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b3b3r/demo/CsvToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package b3b3r.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
