ccb08a510589f2184ea518019b8fa99c0b956a99
b3b3r <b3b3rseg@gmail.com>
2021-11-13 17:31:43 +0100

    add job and step into config

--- src/main/java/b3b3r/demo/config/SpringBatchConfig.java
+++ src/main/java/b3b3r/demo/config/SpringBatchConfig.java
@@ -1,2 +1,35 @@
-package b3b3r.demo.config;public class SpringBatchConfig {
+package b3b3r.demo.config;
+
+import b3b3r.demo.model.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBatchConfig {
+
+    @Bean
+    Job job(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory,
+            ItemReader<User> itemReader, ItemProcessor<User,User> itemProcessor,
+            ItemWriter<User> itemWriter
+            ){
+        Step step = stepBuilderFactory.get("ETL-file-load")
+            .<User, User>chunk(100)
+            .reader(itemReader)
+            .processor(itemProcessor)
+            .writer(itemWriter)
+            .build();
+
+        return jobBuilderFactory.get("ETL-Load")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
 }
